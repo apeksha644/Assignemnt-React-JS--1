c1aeee1ea64a09f28e3c29a23300af0188535516
apeksha644 <58803047+apeksha644@users.noreply.github.com>
2021-10-04 13:01:01 +0530

    Create Welcome.js

---
+++ src/components/Welcome.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Welcome = ({name}) => {
+    let sayHi = `Hey ${name}!`;
+    let greetMessage = `Welcome to Newton School.`;
+    return (
+        <div>
+            <h1>{sayHi}</h1>
+            <h2>{greetMessage}</h2>
+        </div>
+    );
+};
+
+export default Welcome;
